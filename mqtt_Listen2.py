c809ed20fc4a067a30da7dd6ec703102f20e08cd
shitakhaoboo <38935013+shitakhaoboo@users.noreply.github.com>
2020-08-29 16:41:52 +0300

    Update mqtt_Listen2.py
    update broker to broker.hivemq.com because mosquitto was down

--- mqtt_Listen2.py
+++ mqtt_Listen2.py
@@ -112,5 +112,5 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("test.mosquitto.org", 1883)
+client.connect("broker.hivemq.com", 1883)
 client.loop_forever()
