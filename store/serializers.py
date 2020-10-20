dcad96bacabfe21ed0d25e68be6a5d83feef11ed
shitakhaoboo <38935013+shitakhaoboo@users.noreply.github.com>
2020-10-20 11:47:10 +0300

    Update serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import load_meter
+from .models import load_meter, daily_consumption
 
 
 
@@ -9,3 +9,9 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = load_meter
         fields = ('id','meter','ksh','token','units','day','time')
+  
+
+class ConsumptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = daily_consumption
+        fields = ('id','meter','usage','day')
